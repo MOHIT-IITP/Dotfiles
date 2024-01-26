fe453c9fbd9643e51c1eebd8772a2fa15b453de8
MOHIT-IITP <codingbymohit@gmail.com>
2024-01-26 15:48:09 +0000

    Add keymap.lua

---
+++ Neovim/keymaps.lua
@@ -0,0 +1,11 @@
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.opt.updatetime = 50
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
