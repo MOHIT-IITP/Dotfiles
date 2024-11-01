c44fde9b532e983d61ef01450ec37fd6ed813055
MOHIT-IITP <codingbymohit@gmail.com>
2024-11-01 09:45:23 +0530

    major update

--- Neovim/README.md
+++
@@ -1,4 +0,0 @@
-<h1>Pre required things </h1>
-<ol>
-  <li>Neovim(latest version)</li>
-</ol>
--- get_start_with_linux/01.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S xwallpaper python-pywal python-pywalfox neovim vim nano rofi pavucontrol sxhkd nautilus screenkey xorg-xmodmap xorg-xrandr
