e3dbad3b1cf64eb687131b5bffdb32be80a5cbbe
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-07-07 09:20:26 +0530

    Added: terminal tools

--- config/mouseSetting.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xinput --set-prop 'pointer:''Logitech G102 LIGHTSYNC Gaming Mouse' 'libinput Accel Profile Enabled' 0, 1
-xinput --set-prop 'pointer:''Logitech G102 LIGHTSYNC Gaming Mouse' 'libinput Accel Speed' -0.5
